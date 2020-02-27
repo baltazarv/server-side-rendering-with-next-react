afb4a11fd17c98df6ed9f525379f08774acedd56
Baltazar Villegas <baltazarv@gmail.com>
2020-02-27 06:43:33 -0500

    Router used instead of withRouter on pages.

--- pages/clinics/index.jsx
+++ pages/clinics/index.jsx
@@ -1,14 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import Router from 'next/router';
 import MainLayout from '../../components/layouts/MainLayout';
 import Message from '../../components/includes/Message';
 
-const Clinics = () => (
-  <>
-    <MainLayout>
-      <h2>Clinics</h2>
-      <Message />
-    </MainLayout>
-  </>
-);
+const Clinics = () => {
+
+  // console.log(Router);
+  // console.log(Router.pathname);
+
+  useEffect(() => {
+    // when hit the back button
+    //   Router.beforePopState(({ url, as, options }) => {
+    //     if (as === '/clinics') {
+    //       // forces a reload
+    //       window.location.href = as;
+    //       return false;
+    //     }
+    //     return true;
+    //   });
+
+    Router.replace('/clinics', '/clinics?clinic=tnc');
+  });
+
+  return (
+    <>
+      <MainLayout>
+        <h2>Clinics</h2>
+        <Message />
+        <br />
+        <button onClick={() => Router.push('/')} type="button">Go Home</button>
+      </MainLayout>
+    </>
+  );
+};
 
 export default Clinics;
